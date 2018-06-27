0de4b1ae96a3dcb67cbaf68b8afeffe95a7652cf
Theophelus <aneletom10@gmail.com>
2018-06-27 16:20:19 +0200

    adding google fonts and tests for adding new shoe

--- js/shoe_catalogue_logic.js
+++ js/shoe_catalogue_logic.js
@@ -5,7 +5,7 @@
 // TODO: 5 Find a way to filter by size,color, and brand: doing and get shoe associated to the description: Done
 // TODO: 6 The program must filter by color or brand or size and return all the shoes that contain that color: IN PROGRESS
 // TODO: 7 When the search button is being pressed then return the decsription of that partivular shoe
-// var Shoe_Catalogue = function() {
+var Shoe_Catalogue = function() {
 var shoeData = [
     { color: 'Brown', brand: 'Kurt Geiger', price: 1000, size: 39, in_stock: 6 },
     { color: 'Black', brand: 'Europa Art', price: 1500, size: 40, in_stock: 3 },
@@ -15,34 +15,34 @@ var shoeData = [
     { color: 'Brown', brand: 'Kurt Geiger', price: 2000, size: 40, in_stock: 8 }
 ];
 console.log(shoeData);
-// var filter = function(brand, color, size) {
-//     // difine and empty array to filtered results
-//     var filteredResults = [];
-//     for (var i = 0; i < shoeData.length; i++) {
-//         var currentData = shoeData[i];
-//         if (currentData.color == color && currentData.brand == brand && currentData.size == size) {
-//             filteredResults.push(currentData);
-//             console.log(currentData);
-//             console.log(filteredResults);
-//         }
+var filter = function(brand, color, size) {
+    // difine and empty array to filtered results
+    var filteredResults = [];
+    for (var i = 0; i < shoeData.length; i++) {
+        var currentData = shoeData[i];
+        if (currentData.color == color && currentData.brand == brand && currentData.size == size) {
+            filteredResults.push(currentData);
+            console.log(currentData);
+            console.log(filteredResults);
+        }
 
-//         // if(brand == currentData.brand && color== '' && size == ''){
-//         //   if (brand == '' && color == currentData.color && size == '') {
-//         //     if (brand == '' && color == '' && size == currentData.size) {
-//         //         filteredResults.push(currentData);
-//         //           //console.log(filteredResults);
-//         //     }
-//         //     else {
-//         //       return false;
-//         //     }
-//         //   }
-//         // }
-//         // if (brand == currentData.brand && color == '' && size == currentData.size) {
-//         //     filteredResults.push(current)
-//         // }
-//         return filteredResults;
-//     }
-// };
+        // if(brand == currentData.brand && color== '' && size == ''){
+        //   if (brand == '' && color == currentData.color && size == '') {
+        //     if (brand == '' && color == '' && size == currentData.size) {
+        //         filteredResults.push(currentData);
+        //           //console.log(filteredResults);
+        //     }
+        //     else {
+        //       return false;
+        //     }
+        //   }
+        // }
+        // if (brand == currentData.brand && color == '' && size == currentData.size) {
+        //     filteredResults.push(current)
+        // }
+        return filteredResults;
+    }
+};
 // Create a function to add new shoes in an shoe shoeData
 var newShoe = function(newColor, newBrand, newPrice, newSize, newStock) {
     for (var i = 0; i < shoeData.length; i++) {
@@ -59,20 +59,21 @@ var newShoe = function(newColor, newBrand, newPrice, newSize, newStock) {
         brand: newBrand,
         size: newSize,
         price: newPrice,
-        in_stock: newStock,
+        in_stock: newStock
     });
+    return currentData;
 };
-newShoe('Red', 'XO Royalty', 2000, 40, 2);
+//Checkig if shoe exist, if exist increment in_stock
 newShoe('Blue', 'XO Royalty', 2000, 39, 10);
-newShoe('White', 'Nike', 250, 41, 4);
-newShoe('Black', 'Adidas', 300, 40, 10);
-newShoe('Black', 'Adidas', 300, 40, 2);
-// console.log(shoeData[2]);
-console.log(shoeData[4]);
+//adding new shoe
+// newShoe('Nevy', 'XO Royalty', 2500, 41, 4);
+// newShoe('Black', 'Adidas', 300, 40, 2);
+console.log(shoeData[2]);
+// console.log(shoeData[4]);
 console.log(shoeData);
-// return {
-//     filter,
-//     newShoe,
-//     getShoeData
-// };
-// }
+return {
+    // filter,
+    newShoe,
+    // getNewShoe
+};
+}
--- tests/tests.js
+++ tests/tests.js
@@ -1,41 +1,19 @@
-// var shoeData = [
-//     { color: 'Brown', brand: 'Kurt Geiger', price: 1000, size: 39, in_stock: 6 },
-//     { color: 'Black', brand: 'Europa Art', price: 1500, size: 40, in_stock: 3 },
-//     { color: 'Blue', brand: 'XO Royalty', price: 2000, size: 39, in_stock: 8 },
-//     { color: 'Black', brand: 'Aldo', price: 2000, size: 39, in_stock: 2 },
-//     { color: 'Red', brand: 'XO Royalty', price: 2000, size: 40, in_stock: 10 },
-//     { color: 'Brown', brand: 'Kurt Geiger', price: 2000, size: 40, in_stock: 8 }
-// ];
-
 describe('shoe Catalogue widget', function(){
-  // it('should return color, brand and size of a shoe matching a description', function(){
-  //   var shoeCatalogue = Shoe_Catalogue();
-  //   console.log(shoeCatalogue.filter('XO Royalyt', 'Blue', 39));
-  //   assert.deepEqual([{color: "Blue", brand: "XO Royalty", price: 2000, size: 39, in_stock: 8}], shoeCatalogue.filter('XO Royalty', 'Blue', 39));
-  // });
-  //
-  // it('should be able to filter by color and return all the shoes related to that color', function(){
-  //   var shoeCatalogue = Shoe_Catalogue()
-  //   console.log(shoeCatalogue.filter());
-  //   assert.deepEqual([{color: 'Brown', brand: 'Kurt Geiger', price: 2000, size: 40, in_stock: 8 }], shoeCatalogue.filter('Kurt Geiger', '', ''));
-  // });
-  // it('should be able to filter by color and return all the shoes related to that color', function(){
-  //   var shoeCatalogue = Shoe_Catalogue()
-  //   shoeCatalogue.newShoe('White', 'Nike', 1000, 6, 8);
-  //   console.log(shoeCatalogue.getShoeData())
-  //   assert.deepEqual('', shoeCatalogue.getShoeData());
-  // });
-  it('should be able to filter by color and return all the shoes related to that color', function(){
-    var shoeCatalogue = Shoe_Catalogue()
-    shoeCatalogue.newShoe('Blue', 'XO Royalty', 39);
-    console.log(shoeCatalogue.getShoeData())
-    assert.deepEqual([
+  it('should to increment the stock if a shoe already exists', function(){
+    var shoeCatalogue = Shoe_Catalogue();
+    assert.deepEqual(
+    [
         { color: 'Brown', brand: 'Kurt Geiger', price: 1000, size: 39, in_stock: 6 },
         { color: 'Black', brand: 'Europa Art', price: 1500, size: 40, in_stock: 3 },
-        { color: 'Blue', brand: 'XO Royalty', price: 2000, size: 39, in_stock: 18 },
+        { color: 'Blue', brand: 'XO Royalty', price: 2000, size: 39, in_stock: 8 },
         { color: 'Black', brand: 'Aldo', price: 2000, size: 39, in_stock: 2 },
         { color: 'Red', brand: 'XO Royalty', price: 2000, size: 40, in_stock: 10 },
         { color: 'Brown', brand: 'Kurt Geiger', price: 2000, size: 40, in_stock: 8 }
-    ], shoeCatalogue.getShoeData());
+        // { color: 'Nevy', barnd: 'XO Royalty', price: 2500, size: 39, in_stock: 10 }
+    ], shoeCatalogue.newShoe('Blue', 'XO Royalty', 2000, 39, 10));
   });
+  // it('should be able to increment the size of a shoe it already axists', function(){
+  //   var shoeCatalogue = Shoe_Catalogue();
+  //
+  // });
 });
