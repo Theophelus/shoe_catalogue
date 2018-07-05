d024f7ecf418251d175affecd662173c2284c34e
Theophelus <aneletom10@gmail.com>
2018-07-05 17:25:41 +0200

    addig to functions for side bar navigations

--- js/catalogueHandlebars.js
+++ js/catalogueHandlebars.js
@@ -1,10 +1,16 @@
 document.addEventListener('DOMContentLoaded', function() {
   document.querySelector('.fa-plus-square').addEventListener('click', function(){
     document.querySelector('.top-bar').style.height = '65%';
-    document.querySelector('.top-bar').style.height = 'transition: all 0.3s ease-in-out';
   });
   document.querySelector('.fa-window-close').addEventListener('click', function(){
     document.querySelector('.top-bar').style.height = '0%';
+  });
+
+  document.querySelector('.closebtn').addEventListener('click', function(){
+    document.querySelector('.side-bar').style.width = '0px';
+  });
+  document.querySelector('.hamburger').addEventListener('click', function(){
+    document.querySelector('.side-bar').style.width = '250px';
   });
 
 });
