a1de3318ea47737e32b73745b3c2def907b2c939
Theophelus <aneletom10@gmail.com>
2018-07-06 13:13:41 +0200

    adding main section for shoes

--- js/catalogueHandlebars.js
+++ js/catalogueHandlebars.js
@@ -8,9 +8,11 @@ document.addEventListener('DOMContentLoaded', function() {
 
   document.querySelector('.closebtn').addEventListener('click', function(){
     document.querySelector('.side-bar').style.width = '0px';
+    document.querySelector('.main').style.width = '250px';
   });
   document.querySelector('.hamburger').addEventListener('click', function(){
     document.querySelector('.side-bar').style.width = '250px';
+    document.querySelector('.main').style.width = '250px';
   });
 
 });
